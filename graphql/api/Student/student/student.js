fc6f29349635077890af4c6d342a1b57c86dd51a
YERIM <dnaer456@gmail.com>
2020-11-25 21:21:01 +0900

    Query.Mutation

--- graphql/api/Student/student/student.js
+++ graphql/api/Student/student/student.js
@@ -14,5 +14,62 @@ export default {
         return [];
       }
     },
+    Mutation: {
+      createStudent: async (_, args) => {
+        try {
+          const { name, age, school, gender, regeon } = args;
+
+          const result = await Student.create({
+            name,
+            age,
+            school,
+            gender,
+            regeon,
+          });
+          console.log(result);
+
+          return true;
+        } catch (e) {
+          console.log(e);
+          return false;
+        }
+      },
+      deleteStudent: async (_, args) => {
+        const { id } = args;
+
+        try {
+          const result = await Student.deleteOne({ _id: id });
+          console.log(result);
+
+          return true;
+        } catch (e) {
+          console.log(e);
+          return false;
+        }
+      },
+      updateStudent: async (_, args) => {
+        const { id } = args;
+
+        try {
+          const { age, school, regeon } = args;
+          const result = await Student.updateOne(
+            { _id: id },
+            {
+              $set: {
+                age,
+                school,
+                regeon,
+              },
+            }
+          );
+          // find({},{특별히 조회할 애를 넣는 곳 넣을 것이 있으면 !})
+          console.log(result);
+          return true;
+        } catch (e) {
+          console.log(e);
+          return false;
+        }
+      },
+    },
   },
 };
